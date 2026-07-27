ddf0255a6dc4afbdf2dc10065023a962654a384a
asynchronousRohit <rohit.pokhariya123@gmail.com>
2026-07-27 10:25:00 +0530

    Check Palindrome completed

---
+++ q6.js
@@ -0,0 +1,22 @@
+// Check Palindrome Number
+let prompt = require("prompt-sync")();
+const input = Number(prompt("Enter a number"));
+
+const checkPalindrome = (n) =>{
+    // 121
+    let divide = n;
+    let res = 0;
+    let isPalindrome = false;
+    while(divide > 0){
+        let remainder = divide%10;
+        divide = Math.floor(divide/10);
+        res = res * 10 + remainder;
+        // console.log(res);
+        if(res === n) isPalindrome = true;
+    }
+    isPalindrome ? console.log("Palindrome") : console.log("Not Palindrome")
+   
+
+
+}
+checkPalindrome(input)
